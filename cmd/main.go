a20988854103f2614f81b535b351e3d2e35c84a0
nidal_sentoke <nidalakhmedjanov@gmail.com>
2026-06-06 19:11:12 +0500

    add server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	s "github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "app-", log.Ldate|log.Ltime|log.Lshortfile|log.LstdFlags)
+	server := s.NewServer(logger)
+
+	err := server.Server.ListenAndServe()
+	if err != nil {
+		server.Logger.Fatal("Server failed to start: " + err.Error())
+	}
 }
