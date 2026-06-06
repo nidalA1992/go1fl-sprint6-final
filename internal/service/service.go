ed663f88218a217515fb6041fe89e65ea79fa1df
nidal_sentoke <nidalakhmedjanov@gmail.com>
2026-06-06 17:41:07 +0500

    add services

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+var errEmptyStr = errors.New("empty string")
+
+func isMorseStr(str string) bool {
+	return strings.ToUpper(str) == strings.ToLower(str)
+}
+
+func HandleStr(str string) (string, error) {
+	if str == "" {
+		return "", errEmptyStr
+	}
+
+	if isMorseStr(str) {
+		return morse.ToText(str), nil
+	}
+
+	return morse.ToMorse(str), nil
+}
